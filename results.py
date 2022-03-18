79e4ad9e24bcec10f18823fe2b06c847d16cc7f0
georgia fisher <g.fisher@neura.edu.au>
2022-03-18 11:25:13 +1100

    added correlation between low and high level proprio

--- results.py
+++ results.py
@@ -56,3 +56,51 @@ def _make_test_session_list(n_participants):
             test_session.append('B')
 
     return test_session
+
+
+def correlate_low_and_high_areas(all_subject_data):
+    width_to_width_areas = []
+    line_to_width_areas = []
+    width_to_line_areas = []
+
+    for participant in all_subject_data:
+        width_to_width = calculate_area.normalised(participant.WIDTH_WIDTH.ACTUAL, participant.WIDTH_WIDTH.PERCEIVED, 'exp2')
+        line_to_width = calculate_area.normalised(participant.LINE_WIDTH.ACTUAL, participant.LINE_WIDTH.PERCEIVED, 'exp2')
+        width_to_line = calculate_area.normalised(participant.WIDTH_LINE.ACTUAL, participant.WIDTH_LINE.PERCEIVED, 'exp2')
+
+        width_to_width_areas.append(width_to_width)
+        line_to_width_areas.append(line_to_width)
+        width_to_line_areas.append(width_to_line)
+
+    if len(width_to_line_areas) + len(line_to_width_areas) + len(width_to_width_areas) != 90:
+        print('Uneven lists')
+
+    low_high_1 = utils.pearson_r_ci(width_to_width_areas, line_to_width_areas)
+    low_high_2 = utils.pearson_r_ci(width_to_width_areas, width_to_line_areas)
+
+    print(f'Width-width vs. Line-width r: {low_high_1[0]} 95%CI: {low_high_1[1]}')
+    print(f'Width-width vs. Width-line r: {low_high_2[0]} 95%CI: {low_high_2[1]}')
+
+
+def correlate_low_and_high_r2(all_subject_data):
+    width_to_width_r2 = []
+    line_to_width_r2 = []
+    width_to_line_r2 = []
+
+    for participant in all_subject_data:
+        width_to_width = utils.calculate_r2(participant.WIDTH_WIDTH.ACTUAL, participant.WIDTH_WIDTH.PERCEIVED)
+        line_to_width = utils.calculate_r2(participant.LINE_WIDTH.ACTUAL, participant.LINE_WIDTH.PERCEIVED)
+        width_to_line = utils.calculate_r2(participant.WIDTH_LINE.ACTUAL, participant.WIDTH_LINE.PERCEIVED)
+
+        width_to_width_r2.append(width_to_width)
+        line_to_width_r2.append(line_to_width)
+        width_to_line_r2.append(width_to_line)
+
+    if len(width_to_line_r2) + len(line_to_width_r2) + len(width_to_width_r2) != 90:
+        print('Uneven lists')
+
+    low_high_1 = utils.pearson_r_ci(width_to_width_r2, line_to_width_r2)
+    low_high_2 = utils.pearson_r_ci(width_to_width_r2, width_to_line_r2)
+
+    print(f'Width-width vs. Line-width r: {low_high_1[0]} 95%CI: {low_high_1[1]}')
+    print(f'Width-width vs. Width-line r: {low_high_2[0]} 95%CI: {low_high_2[1]}')
