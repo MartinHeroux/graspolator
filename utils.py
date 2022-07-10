ed958e2dbe729e51593927ca9e62022a5323290c
georgia fisher <g.fisher@neura.edu.au>
2022-07-11 07:06:56 +1000

    font strings changed to variables

--- utils.py
+++ utils.py
@@ -385,17 +385,17 @@ def get_directory_list(directory):
 ##################################################################
 
 def set_ax_parameters(ax, x_ticks, y_ticks, x_tick_labels, y_tick_labels, x_lims, y_lims, x_label, y_label, fontsize,
-                      gridlines):
+                      gridlines, font='arial'):
     ax.set_xticks(x_ticks)
-    ax.set_xticklabels(x_tick_labels, fontsize=fontsize, fontfamily='FreeSans')
+    ax.set_xticklabels(x_tick_labels, fontsize=fontsize, fontfamily=font)
     ax.set_yticks(y_ticks)
-    ax.set_yticklabels(y_tick_labels, fontsize=fontsize, fontfamily='FreeSans')
+    ax.set_yticklabels(y_tick_labels, fontsize=fontsize, fontfamily=font)
     ax.set_xlim(x_lims)
     ax.set_ylim(y_lims)
-    ax.set_xlabel(x_label, fontsize=fontsize, fontfamily='FreeSans')
-    ax.set_ylabel(y_label, fontsize=fontsize, fontfamily='FreeSans')
+    ax.set_xlabel(x_label, fontsize=fontsize, fontfamily=font)
+    ax.set_ylabel(y_label, fontsize=fontsize, fontfamily=font)
     if gridlines:
-        ax.grid(axis='both', linewidth=0.5, color='lightgrey')
+        ax.grid(axis='both', linewidth=0.5, color='lightgray')
 
 
 def draw_ax_spines(ax, left, right, top, bottom, x_offset = False, y_offset = False):
