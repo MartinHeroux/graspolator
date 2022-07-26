f0082240b08a3f99afa36868681f7c0bba95fb52
georgia fisher <g.fisher@neura.edu.au>
2022-07-26 14:06:33 +1000

    calls to functions to plot individual subject scatter and error added

--- main_script.py
+++ main_script.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import data
 import figures
+import plot
 import results
 
 #####################
@@ -18,6 +19,10 @@ all_subject_names_exp1 = data.return_all_subject_IDs(exp_1, exp_1_data_directory
 
 #   Write all results to text file
 results.write_all(all_subject_data_exp1, all_subject_names_exp1, exp_1, exp_1_data_directory)
+
+#   Generate individual subject plots
+plot.individual_scatterplots(all_subject_data_exp1, all_subject_names_exp1, exp_1)
+plot.individual_error_plots(all_subject_data_exp1, all_subject_names_exp1, exp_1)
 
 #   Generate all figures
 figures.generate_all(all_subject_data_exp1, all_subject_names_exp1, exp_1)
@@ -38,6 +43,10 @@ all_subject_names_exp2 = data.return_all_subject_IDs(exp_2, exp_2_data_directory
 
 #   Write all results to text file
 results.write_all(all_subject_data_exp2, all_subject_names_exp2, exp_2, exp_2_data_directory)
+
+#   Generate individual subject plots
+plot.individual_scatterplots(all_subject_data_exp2, all_subject_names_exp2, exp_2)
+plot.individual_error_plots(all_subject_data_exp2, all_subject_names_exp2, exp_2)
 
 #   Generate all figures
 figures.generate_all(all_subject_data_exp2, all_subject_names_exp2, exp_2)
