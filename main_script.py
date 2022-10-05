d1c570003451c5a566e81c84f0af7660a60c367f
georgia fisher <g.fisher@neura.edu.au>
2022-10-05 13:23:25 +1100

    removed unnecessary import statement

--- main_script.py
+++ main_script.py
@@ -11,20 +11,20 @@ import results
 exp_1 = "exp2"
 exp_1_data_directory = Path("./data/exp2")
 
-#   Store all subject data and subject names
+#   Store subject data and subject names
 all_subject_data_exp1 = data.return_all_subject_data(
     exp_1, exp_1_data_directory
 )
 all_subject_names_exp1 = data.return_all_subject_IDs(exp_1, exp_1_data_directory)
 
-#   Write all results to text file
+#   Write results to text file
 results.write_all(all_subject_data_exp1, all_subject_names_exp1, exp_1, exp_1_data_directory)
 
 #   Generate individual subject plots
 plot.individual_scatterplots(all_subject_data_exp1, all_subject_names_exp1, exp_1)
 plot.individual_error_plots(all_subject_data_exp1, all_subject_names_exp1, exp_1)
 
-#   Generate all figures
+#   Generate figures
 figures.generate_all(all_subject_data_exp1, all_subject_names_exp1, exp_1, True)
 
 
@@ -35,18 +35,18 @@ exp_2 = "exp1"
 exp_2_data_directory = Path("./data/exp1")
 exp_2_result_filepath = Path("./results_exp1.txt")
 
-#   Store all subject data and subject names
+#   Store subject data and subject names
 all_subject_data_exp2 = data.return_all_subject_data(
     exp_2, exp_2_data_directory
 )
 all_subject_names_exp2 = data.return_all_subject_IDs(exp_2, exp_2_data_directory)
 
-#   Write all results to text file
+#   Write results to text file
 results.write_all(all_subject_data_exp2, all_subject_names_exp2, exp_2, exp_2_data_directory)
 
 #   Generate individual subject plots
 plot.individual_scatterplots(all_subject_data_exp2, all_subject_names_exp2, exp_2)
 plot.individual_error_plots(all_subject_data_exp2, all_subject_names_exp2, exp_2)
 
-#   Generate all figures
+#   Generate figures
 figures.generate_all(all_subject_data_exp2, all_subject_names_exp2, exp_2, True)
