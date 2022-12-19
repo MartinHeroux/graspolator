28eaa6c5f58de05af962e31098bf1d8bb0126b1b
georgia fisher <g.fisher@neura.edu.au>
2022-12-19 19:45:09 +1100

    created separate experiment 2 processing file

---
+++ run_exp2.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+
+import data
+import figures
+import plot
+import results
+from utils import ExpCodes
+
+import summarise
+
+exp_2 = ExpCodes.EXP_2
+exp2_data_directory = Path(f"./data/{exp_2}")
+
+#   Store subject data and subject names
+all_subject_data_exp2 = data.return_all_subject_data(
+    exp_2, exp2_data_directory
+)
+all_subject_names_exp2 = data.return_all_subject_IDs(exp_2, exp2_data_directory)
+
+#   Write results to text file
+results.write_all(all_subject_data_exp2, all_subject_names_exp2, exp_2, exp2_data_directory)
+
+#   Write individual subject data to .csv
+summarise.write_raw_data_summary_exp2(all_subject_data_exp2, all_subject_names_exp2)
+
+#   Generate individual subject plots
+plot.individual_scatterplots(all_subject_data_exp2, all_subject_names_exp2, exp_2)
+plot.individual_error_plots(all_subject_data_exp2, all_subject_names_exp2, exp_2)
+
+#   Generate figures
+figures.generate_all(all_subject_data_exp2, all_subject_names_exp2, exp_2, True)
