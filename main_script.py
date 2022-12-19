08edff2f1d5ab4a277eeca773bb2a7a8119e748e
georgia fisher <g.fisher@neura.edu.au>
2022-12-19 18:48:21 +1100

    changed function names to more clearly indicate experiment name

--- main_script.py
+++ main_script.py
@@ -4,12 +4,15 @@ import data
 import figures
 import plot
 import results
+from utils import ExpCodes
 
-#####################
-#   EXPERIMENT 1
-#####################
-exp_1 = "exp2"
-exp_1_data_directory = Path("./data/exp2")
+##########################
+#   EXPERIMENT 1 - LOVISA
+##########################
+import summarise
+
+exp_1 = ExpCodes.EXP_1
+exp_1_data_directory = Path(f"./data/{exp_1}'")
 
 #   Store subject data and subject names
 all_subject_data_exp1 = data.return_all_subject_data(
@@ -20,6 +23,9 @@ all_subject_names_exp1 = data.return_all_subject_IDs(exp_1, exp_1_data_directory
 #   Write results to text file
 results.write_all(all_subject_data_exp1, all_subject_names_exp1, exp_1, exp_1_data_directory)
 
+#   Write individual subject data to .csv
+summarise.write_raw_data_summary_exp1(all_subject_data_exp1)
+
 #   Generate individual subject plots
 plot.individual_scatterplots(all_subject_data_exp1, all_subject_names_exp1, exp_1)
 plot.individual_error_plots(all_subject_data_exp1, all_subject_names_exp1, exp_1)
@@ -28,21 +34,23 @@ plot.individual_error_plots(all_subject_data_exp1, all_subject_names_exp1, exp_1
 figures.generate_all(all_subject_data_exp1, all_subject_names_exp1, exp_1, True)
 
 
-#####################
-#   EXPERIMENT 2
-#####################
-exp_2 = "exp1"
-exp_2_data_directory = Path("./data/exp1")
-exp_2_result_filepath = Path("./results_exp1.txt")
+############################
+#   EXPERIMENT 2 - KATHY
+############################
+exp_2 = ExpCodes.EXP_2
+exp2_data_directory = Path(f"./data/{exp_2}")
 
 #   Store subject data and subject names
 all_subject_data_exp2 = data.return_all_subject_data(
-    exp_2, exp_2_data_directory
+    exp_2, exp2_data_directory
 )
-all_subject_names_exp2 = data.return_all_subject_IDs(exp_2, exp_2_data_directory)
+all_subject_names_exp2 = data.return_all_subject_IDs(exp_2, exp2_data_directory)
 
 #   Write results to text file
-results.write_all(all_subject_data_exp2, all_subject_names_exp2, exp_2, exp_2_data_directory)
+results.write_all(all_subject_data_exp2, all_subject_names_exp2, exp_2, exp2_data_directory)
+
+#   Write individual subject data to .csv
+summarise.write_raw_data_summary_exp2(all_subject_data_exp2, all_subject_names_exp2)
 
 #   Generate individual subject plots
 plot.individual_scatterplots(all_subject_data_exp2, all_subject_names_exp2, exp_2)
